0eff869d926c185148cef5ad54d1a2b7aa24f71d
kjunice4 <37306150+kjunice4@users.noreply.github.com>
2024-01-03 19:35:15 -0500

    Update Faker_Data_to_use.py

--- Faker_Data_to_use.py
+++ Faker_Data_to_use.py
@@ -13,7 +13,7 @@ first_name = [fake.first_name() for i in range(N)]
 last_name = [fake.last_name() for i in range(N)]
 office_locations = ["Somerset, NJ","Austin,TX","Piscataway, NJ"]
 office = [random.choice(office_locations) for i in range(N)]
-email = [first_name[i] + "_" + last_name[i] + "@gmail.com" for i in range(N)]
+email = [first_name[i] + "_" + last_name[i] + "@email.com" for i in range(N)]
 sales = [ "$" + str(random.choice(range(1000,1000000))) for i in range(N)]
 
 df_fake = pd.DataFrame({
